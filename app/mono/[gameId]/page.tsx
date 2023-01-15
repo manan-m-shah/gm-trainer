cc6e0054e87a512725c353b22f3c548f2f6cf198
manan-m-shah <shahmananvyara@gmail.com>
2023-01-16 00:22:46 +0530

    🚑 Loading Fix for External Links

--- app/mono/[gameId]/page.tsx
+++ app/mono/[gameId]/page.tsx
@@ -6,6 +6,7 @@ import RandomBoard from "../../../components/Chess Boards/RandomBoard";
 import DefaultController from "../../../components/Controller/DefaultController";
 import SinglePlayerBoard from "../../../components/Chess Boards/SinglePlayerBoard";
 import { getSinglePlayerGame } from "../../../apis/singlePlayerGames";
+import { ActionKind } from "../../../types/Context";
 
 type Props = {
   params: {
@@ -16,6 +17,18 @@ type Props = {
 const SinglePlayerGame = ({ params }: Props) => {
   const { state, dispatch } = useContext(AppContext);
   console.log(params.gameId);
+
+  //* If the game id is not set in the context, set it
+  // useful for when the user refreshes the page
+  // or when the user onboards through a link
+  useEffect(() => {
+    if (!state.uid) {
+      dispatch({
+        type: ActionKind.SET_UID,
+        payload: params.gameId,
+      });
+    }
+  }, [params.gameId]);
 
   return (
     <div className="game-div">
